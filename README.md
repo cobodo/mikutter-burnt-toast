b93c06e27396b084e326d60264e323d912bb49f8
cobodo <cobodo@gmail.com>
2018-03-09 02:38:42 +0900

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# mikutter BurntToastプラグイン
+PowerShellから通知を送るユーティリティコマンド [BurntToast](https://github.com/Windos/BurntToast) を利用して、WSL上で動くmikutterからWindows10へ通知を送るプラグインです。
+
+なお、中身はmikutter本体同梱の `core/plugin/libnotify/notify-send.rb` とほぼ同じです。
+
+## 使い方
+まずPowershellを管理者権限で開き、以下のコマンドを実行してBurntToastをインストールします。
+
+```powershell
+> Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+> Install-Module -Name BurntToast
+```
+
+次にWSL上で以下のコマンドを実行して、このプラグインをmikutterにインストールします。
+
+```shell-session
+$ mkdir -p ~/.mikutter/plugin && git clone git://github.com/cobodo/mikutter-burnt-toast ~/.mikutter/plugin/burnt_toast
+```
+
+設定＞BurntToastから、Windows側から見たWSLのrootfsへのパスを設定してください。筆者の環境では以下のような値でした。
+
+`C:\Users\cobodo\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs`
+
---
+++ burnt_toast.rb
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create(:burnt_toast) do
+  on_popup_notify do |user, text, &stop|
+    icon_path(user.icon).trap{|err|
+      warn err
+      icon_path(Skin['notfound.png'])
+    }.next{|icon_file_name|
+      command = ["powershell.exe", "-command", "New-BurntToastNotification", "-Text"]
+      if(text.is_a? Diva::Model)
+        text = "'#{text.to_s}'"
+      end
+      if user
+        command << "'#{user.title}',"
+      end
+      command << "'#{text}'"
+      if user
+        win_path = wsl_path_to_windows_path(icon_file_name)
+        command << "-AppLogo" << "'#{win_path}'"
+      end
+      bg_system(*command)
+    }.terminate
+    stop.call
+  end
+
+  def wsl_path_to_windows_path (wsl_path)
+    if wsl_path.start_with?('/mnt/') && wsl_path[6] == '/'
+      drv = wsl_path[5].upcase
+      path = wsl_path[7..-1]
+      "#{drv}:#{path}".gsub(%r|/|) do "\\" end
+    else
+      "#{UserConfig[:burnt_toast_rootfs_path]}#{wsl_path}".gsub(%r|/|) do "\\" end
+    end
+  end
+
+  def icon_path(photo)
+    ext = photo.uri.path.split('.').last || 'png'
+    fn = File.join(icon_tmp_dir, Digest::MD5.hexdigest(photo.uri.to_s) + ".#{ext}")
+    Delayer::Deferred.new.next{
+      case
+      when FileTest.exist?(fn)
+        fn
+      else
+        photo.download_pixbuf(width: 48, height: 48).next{|p|
+          FileUtils.mkdir_p(icon_tmp_dir)
+          photo.pixbuf(width: 48, height: 48).save(fn, 'png')
+          fn
+        }
+      end
+    }
+  end
+
+  memoize def icon_tmp_dir
+    File.join(Environment::TMPDIR, 'burnt_toast', 'icon').freeze
+  end
+
+  UserConfig[:burnt_toast_rootfs_path] = ''
+
+  settings "BurntToast" do
+    input 'Windows側から見たWSL rootfsの位置', :burnt_toast_rootfs_path
+  end
+end
